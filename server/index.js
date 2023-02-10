21238cf5411a7435f4bf8884d85842565c9294e1
Aashish Katila <aashishkatila@gmail.com>
2023-02-10 12:17:22 +0545

    db connected

--- server/index.js
+++ server/index.js
@@ -2,15 +2,28 @@ const express = require('express');
 
 const app = express();
 
+const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors') // To access backend API
+
+const  db = mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"contact"
+})
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res) =>{
-    res.send("Hello Express");
+    const sqlInsert = "INSERT INTO contact_db (name,email,contact) VALUES ('John','john@gmail.com',24242424)";
+    db.query(sqlInsert,(err,result ) =>{
+        console.log("Error",err)
+        console.log("Result = ",result)
+        res.send("Hello Express"); 
+    })
 })
 
 app.listen(5000,()=>{
