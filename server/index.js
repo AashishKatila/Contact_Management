ebadb6b96780628ca156dd1f37e078d890a3ccbf
Aashish Katila <aashishkatila@gmail.com>
2023-02-10 11:59:13 +0545

    initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors') // To access backend API
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res) =>{
+    res.send("Hello Express");
+})
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
+})
