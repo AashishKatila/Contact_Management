4b0d19f846f49cb1cb51ed2cd10447e2bc2680f6
Aashish Katila <aashishkatila@gmail.com>
2024-12-24 18:09:45 +0545

    Fix vercel.json

--- server/index.js
+++ server/index.js
@@ -1,29 +1,83 @@
 const express = require("express");
-const mysql = require("mysql2");
-const cors = require("cors");
 
 const app = express();
 
-// MySQL connection pool
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // To access backend API
+
 const db = mysql.createPool({
-  host: "localhost", 
-  user: "root", 
-  password: "", 
-  database: "contact", 
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "contact",
 });
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/get", (req, res) => {
   const sqlGet = "SELECT * FROM contact_db";
   db.query(sqlGet, (error, result) => {
+    res.send(result);
+  });
+});
+
+app.post("/api/post", (req, res) => {
+  const { name, email, contact } = req.body;
+  const sqlInsert =
+    "INSERT INTO contact_db (name,email,contact) VALUES (?,?,?)";
+  db.query(sqlInsert, [name, email, contact], (error, result) => {
     if (error) {
-      res.status(500).send(error);
-    } else {
-      res.send(result);
+      console.log(error);
     }
   });
 });
 
-module.exports = app;
+app.delete("/api/remove/:id", (req, res) => {
+  const { id } = req.params;
+  const sqlRemove = "DELETE FROM contact_db where id = ?";
+  db.query(sqlRemove, id, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.send({ message: "Contact deleted successfully" });
+    }
+  });
+});
+
+app.get("/api/get/:id", (req, res) => {
+  const { id } = req.params;
+  const sqlGet = "SELECT * FROM contact_db WHERE id =?";
+  db.query(sqlGet, id, (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+    res.send(result);
+  });
+});
+
+app.put("/api/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email, contact } = req.body;
+  const sqlUpdate = "UPDATE contact_db SET name = ?, email = ?,contact = ? WHERE id =?";
+  db.query(sqlUpdate, [name,email,contact,id], (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+    res.send(result);
+  });
+});
+
+app.get("/", (req, res) => {
+  //     const sqlInsert = "INSERT INTO contact_db (name,email,contact) VALUES ('John','john@gmail.com',24242424)";
+  //     db.query(sqlInsert,(err,result ) =>{
+  //         console.log("Error",err)
+  //         console.log("Result = ",result)
+  //         res.send("Hello Express");
+  //     })
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
