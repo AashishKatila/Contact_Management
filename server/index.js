a0c9def659da688573ebc91508c93436acd60ef7
Aashish Katila <aashishkatila@gmail.com>
2024-12-24 18:33:34 +0545

    Fix index.js

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ const cors = require("cors"); // To access backend API
 const serverless = require("serverless-http");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "contact",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 app.use(cors());
